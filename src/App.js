3cd6ccca02f48f822381dc98baa386b652454884
truelivingvip <niteshmechanical629@gmail.com>
2026-02-05 13:39:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
+import {Routes,Route} from 'react-router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AdminDashboard from './AdminDashboard';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-     <Footer></Footer>
+      <AdminDashboard></AdminDashboard>
     </div>
   );
 }
