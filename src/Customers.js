277578092530c4b0f12369cbf3775199c9c1b86d
truelivingvip <niteshmechanical629@gmail.com>
2026-02-07 20:41:39 +0530

    Update Customers.js

--- src/Customers.js
+++ src/Customers.js
@@ -1,8 +1,108 @@
 import React from 'react'
-import { Col, Container, Row, Breadcrumb } from 'react-bootstrap'
+import { Col, Container, Row, Breadcrumb, Table } from 'react-bootstrap'
 import { Link } from 'react-router'
 import LeftNav from './LeftNav'
 const Customers = () => {
+    const customers = [
+        {
+            "id": 1,
+            "name": "Vipul Kumar",
+            "mobile": 7061569926,
+            "e-mail": "vipul@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 2,
+            "name": "Yash Kumar",
+            "mobile": 9122278602,
+            "e-mail": "yash@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 3,
+            "name": "Vidut Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        },
+        {
+            "id": 4,
+            "name": "Adarsh Kumar",
+            "mobile": 7061569926,
+            "e-mail": "niteshmechanical629@gmail.com",
+            "status": "Active"
+        }
+    ]
     return (
         <div>
             <section>
@@ -21,6 +121,33 @@ const Customers = () => {
 
                                         <Breadcrumb.Item active>Customers</Breadcrumb.Item>
                                     </Breadcrumb>
+                                    <Table striped bordered hover>
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Name</th>
+                                                <th>Mobile</th>
+                                                <th>E-mail</th>
+                                                <th>Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {
+                                                customers.map((customer, index) => {
+                                                    return (
+                                                        <tr>
+                                                            <td>{customer.id}</td>
+                                                            <td>{customer.name}</td>
+                                                            <td>{customer.mobile}</td>
+                                                            <td>{customer['e-mail']}</td>
+                                                            <td>{customer.status}</td>
+                                                        </tr>
+                                                    )
+                                                }
+                                                )
+                                            }
+                                        </tbody>
+                                    </Table>
                                 </Col>
                             </Row>
                         </Col>
