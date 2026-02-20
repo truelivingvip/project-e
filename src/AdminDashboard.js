3fd4cbd4f7e27c655c3445c27249d342cb96c4a5
truelivingvip <niteshmechanical629@gmail.com>
2026-02-20 15:19:49 +0530

    .

--- src/AdminDashboard.js
+++ src/AdminDashboard.js
@@ -2,19 +2,51 @@ import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import LeftNav from './LeftNav'
 
+
+
 const AdminDashboard = () => {
+const config = {
+  type: 'polarArea',
+  data: data,
+  options: {}
+},
+const data = {
+  labels: [
+    'Red',
+    'Green',
+    'Yellow',
+    'Grey',
+    'Blue'
+  ]
   return (
     <div>
       <section>
         <Container>
-            <Row>
-                <Col md={3}>
-                <LeftNav></LeftNav>
-                </Col>
-                <Col md={9}>
-                <p>Rightside</p>
-                </Col>
-            </Row>
+          <Row>
+            <Col md={3}>
+              <LeftNav></LeftNav>
+            </Col>
+            <Col md={9}>
+              new Chart(ctx, {
+                type: 'bar',
+              data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+              datasets: [{
+                label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              borderWidth: 1
+      }]
+    },
+              options: {
+                scales: {
+                y: {
+                beginAtZero: true
+        }
+      }
+    }
+  });
+            </Col>
+          </Row>
         </Container>
       </section>
     </div>
