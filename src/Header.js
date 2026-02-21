9b208fcfd885b58b67e12c10b98f44514374a27a
truelivingvip <niteshmechanical629@gmail.com>
2026-02-21 20:01:43 +0530

    cus page

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,14 @@
 import React from 'react'
-import { Col, Container, Row, Form } from 'react-bootstrap'
+import { Col, Container, Row, Form, Button } from 'react-bootstrap'
 import snap from './snap.png'
+import { TfiBasketball } from "react-icons/tfi";
+import { TfiHelpAlt } from "react-icons/tfi";
+import { RiShoppingBag3Line } from "react-icons/ri";
+import { BsCartDash } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
+import { MdArrowOutward } from "react-icons/md";
+
+
 const Header = () => {
   return (
     <div>
@@ -19,9 +27,9 @@ const Header = () => {
             <Col>
               <div className='our'>
                 <ul>
-                  <li><a href='#'>Our Blog</a></li>
-                  <li><a href='#'>Help Center</a></li>
-                  <li><a href='#'>Sell on Snapdeal</a></li>
+                  <li><a href='#'><RiShoppingBag3Line/> Sell on Snapdeal</a></li>
+                  <li><a href='#'><TfiHelpAlt/> Help Center</a></li>
+                  <li><a href='#'><TfiBasketball/> Our Blog</a></li>
                 </ul>
               </div>
             </Col>
@@ -46,15 +54,59 @@ const Header = () => {
             </Col>
             <Col>
               <ul>
-                <li><a href='#'>Login</a></li>
-                <li><a href='#'>My Cart</a></li>
+                <li><a href='#'><BsCartDash/> My Cart</a></li>
+                <li><a href='#'><CgProfile/> Login</a></li>
               </ul>
             </Col>
             <Col>
-
+              <Button className='app' variant="primary" size="lg"><MdArrowOutward/>
+                Download App
+              </Button>
             </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className='cate'>
+        <Container>
+          <Row>
             <Col>
-
+              <ul>
+                <li>
+                  <h6><a href='#'><img src="https://g.sdlcdn.com/imgs/l/c/0/mensnavigationwebhome-92990.jpeg" />Men's Fashion</a></h6>
+                </li>
+                <li>
+                  <h6><a href='#'><img src="https://g.sdlcdn.com/imgs/l/c/1/WoMen_sitenavne-d9da9.jpeg" />
+                    Women's Fashion</a></h6>
+                </li>
+                <li>
+                  <h6><a href='#'><img src="https://g.sdlcdn.com/imgs/l/c/1/homesiteiconne-d6c37.jpeg" />
+                    Home & Kitchen</a></h6>
+                </li>
+                <li>
+                  <h6><a href='#'><img src="https://g.sdlcdn.com/imgs/l/c/1/kidshomeico-c52fa.jpeg" />
+                    Kid's Fashion</a></h6>
+                </li>
+                <li>
+                  <h6><a href='#'><img src="https://g.sdlcdn.com/imgs/l/c/1/Beauty_Site_homene-162a5.jpeg" />
+                    Beauty & Health</a></h6>
+                </li>
+                <li>
+                  <h6><a href='#'><img src="https://g.sdlcdn.com/imgs/l/d/h/Automotive1-56b92.jpg" />
+                    Automotives</a></h6>
+                </li>
+                <li>
+                  <h6><a href='#'><img src="https://g.sdlcdn.com/imgs/l/d/h/MobileAccessories1-32874.jpg" />
+                    Mobile Accessories</a></h6>
+                </li>
+                {/* <li>
+                  <h6><img src="https://g.sdlcdn.com/imgs/l/d/h/elctronic1-cbb06.jpg" />
+                    Electronics</h6>
+                </li>
+                <li>
+                  <h6><img src="https://g.sdlcdn.com/imgs/l/d/h/sportsandfitnes1-b8667.jpg" />
+                    Sports & Fitness</h6>
+                </li> */}
+              </ul>
             </Col>
           </Row>
         </Container>
