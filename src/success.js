04c7167941e96968371615080329e19a79ca904f
truelivingvip <niteshmechanical629@gmail.com>
2026-03-10 16:10:28 +0530

    .

---
+++ src/success.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const success = () => {
+  return (
+    <div>
+      <section>
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Order Placed</h1>
+                </Col>
+            </Row>
+        </Container>
+      </section>
+    </div>
+  )
+}
+
+export default success
