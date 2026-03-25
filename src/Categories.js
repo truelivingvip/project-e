649625ea8e2bf2ab7ad3167e321c77f6528d2d6e
truelivingvip <niteshmechanical629@gmail.com>
2026-03-25 19:28:03 +0530

    .

--- src/Categories.js
+++ src/Categories.js
@@ -5,29 +5,41 @@ import axios from 'axios'
 import { Col, Container, Row, Breadcrumb, Card, Button } from 'react-bootstrap'
 // import { Link } from 'react-router'
 import LeftNav from './LeftNav'
-import { useFormik } from 'formik'
+// import { useFormik } from 'formik'
 import { useDispatch, useSelector } from 'react-redux'
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import { color } from 'chart.js/helpers'
+
 
 const Categories = () => {
+    const CateSchema = Yup.object().shape({
+        categorieName: Yup.string()
+            .min(2, "Too Short!")
+            .max(100, "Too Long!")
+            .required("categorie_Name is required*"),
+        image: Yup.mixed()
+            .required("image is required*"),
+    });
     const dispatch = useDispatch();
-    let navigate=useNavigate();
+    let navigate = useNavigate();
     const { user: currentUser } = useSelector((state) => state.auth)
     console.log(currentUser)
-    useEffect(()=>{
-        currentUser && currentUser.roles[0]==="ROLE_ADMIN"?
-        console.log(currentUser)
-        :
-        navigate('/login');
-    },[currentUser]);
-    useEffect(()=>{
+    // useEffect(() => {
+    //     currentUser && currentUser.roles[0] === "ROLE_ADMIN" ?
+    //         console.log(currentUser)
+    //         :
+    //         navigate('/login');
+    // }, [currentUser]);
+    useEffect(() => {
         axios.get("http://localhost:8090/api/cats")
-            .then(res=>{
+            .then(res => {
                 console.log(res.data);
             })
-        .catch(error =>{
-            console.log("Error-fetching Data");
-        });
-    },[]);
+            .catch(error => {
+                console.log("Error-fetching Data");
+            });
+    }, []);
     const categories = [
         {
             "title": "Men's Fashion",
@@ -41,7 +53,7 @@ const Categories = () => {
             "title": "Home & Kitchen",
             "image": "https://tse3.mm.bing.net/th/id/OIP.AJ1jhYu23jZcW0-l7lRvJAHaHa?rs=1&pid=ImgDetMain&o=7&rm=3"
         },
-        {  
+        {
             "title": "Home & Living",
             "image": "https://i5.walmartimages.com/asr/b38c79ec-09a3-4c23-b88d-9aa6eda2db4e.cca72575f78129850e104c54fa8206b7.jpeg"
         },
@@ -54,15 +66,6 @@ const Categories = () => {
             "image": "https://th.bing.com/th/id/OIP.WAJVvxoRwo1VeMY-VwNTOAHaHa?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3"
         },
     ]
-    const formik = useFormik({
-        initialValues: {
-            categorieName: '',
-            image: '',
-        },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
     return (
         <div>
             <section>
@@ -111,36 +114,51 @@ const Categories = () => {
                         <Col md={2}>
                             <div className='add'>
                                 <h3>Add Categorie</h3>
-                                <form onSubmit={formik.handleSubmit}>
-                                    <Row>
-                                        <Col>
-                                            <label htmlFor="categorieName" className='category'>Categorie Name</label>
-                                            <input
-                                                id="categorieName"
-                                                name="categorieName"
-                                                type="text"
-                                                onChange={formik.handleChange}
-                                                value={formik.values.categorieName}
-                                            />
-                                            {formik.errors.categorieName ? <div>{formik.errors.categorieName}</div> : null}
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
-                                            <label htmlFor="image">Image</label>
-                                            <input
-                                                id="image"
-                                                name="image"
-                                                type="file"
-                                                accept='image/*'
-                                                onChange={formik.handleChange}
-                                                value={formik.values.image}
-                                            />
-                                            {formik.errors.image ? <div>{formik.errors.image}</div> : null}
-                                        </Col>
-                                    </Row>
-                                    <button type="submit">Add</button>
-                                </form>
+                                <Formik
+                                    initialValues={{
+                                        categorieName: '',
+                                        image: '',
+                                    }}
+                                    validationSchema={CateSchema}
+                                    onSubmit={values => {
+                                        // same shape as initial values
+                                        console.log(values);
+                                    }}
+                                >
+                                    {({ errors, touched }) => (
+                                        <Form>
+                                            <Row>
+                                                <Col>
+
+                                                    <label htmlFor="categorieName" className='category'>Categorie Name</label>
+                                                    <input
+                                                        id="categorieName"
+                                                        name="categorieName"
+                                                        type="text"
+                                                    />
+                                                    {errors.categorieName && touched.categorieName ? (
+                                                        <div style={{color:"red"}}>{errors.categorieName}</div>
+                                                    ) : null}
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>
+                                                    <label htmlFor="image">Image</label>
+                                                    <input
+                                                        id="image"
+                                                        name="image"
+                                                        type="file"
+                                                        accept='image/*'
+                                                    />
+                                                    {errors.image && touched.image ? (
+                                                        <div style={{color:'red'}}>{errors.image}</div>
+                                                    ) : null}
+                                                </Col>
+                                            </Row>
+                                            <button type="submit" className='button'>Add</button>
+                                        </Form>
+                                    )}
+                                </Formik>
                             </div>
                         </Col>
                     </Row>
