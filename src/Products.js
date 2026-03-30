001a48672f0c5b6ea0cc63e56f4881a172b33919
truelivingvip <niteshmechanical629@gmail.com>
2026-03-30 13:00:01 +0530

    .

--- src/Products.js
+++ src/Products.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Col, Container, Row, Breadcrumb, Card, Button, Table } from 'react-bootstrap'
 // import { Link } from 'react-router'
 import axios from 'axios';
@@ -16,8 +16,8 @@ import * as Yup from "yup";
 const Products = () => {
     const [categories, setCategories] = useState();
     const [products, setProducts] = useState();
-    
-        const [selectedImages, setSelectedImages] = useState([]);
+
+    const [selectedImages, setSelectedImages] = useState([]);
     const ProductSchema = Yup.object().shape({
         name: Yup.string().required("name is required"),
         price: Yup.number().required("number is required"),
@@ -55,82 +55,85 @@ const Products = () => {
                 console.log("Error-fetching Data");
             });
     }, []);
-    const product = [
-        {
-            "id": 1,
-            "title": "Essence Mascara Lash Princess",
-            "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
-            "category": "beauty",
-            "price": 9.99,
-            "discountPercentage": 10.48,
-            "rating": 2.56,
-            "stock": 99,
-            "tags": [
-                "beauty",
-                "mascara"
-            ],
-            "brand": "Essence",
-            "sku": "BEA-ESS-ESS-001",
-            "weight": 4,
-            "dimensions": {
-                "width": 15.14,
-                "height": 13.08,
-                "depth": 22.99
-            },
-            "warrantyInformation": "1 week warranty",
-            "shippingInformation": "Ships in 3-5 business days",
-            "availabilityStatus": "In Stock",
-            "reviews": [
-                {
-                    "rating": 3,
-                    "comment": "Would not recommend!",
-                    "date": "2025-04-30T09:41:02.053Z",
-                    "reviewerName": "Eleanor Collins",
-                    "reviewerEmail": "eleanor.collins@x.dummyjson.com"
-                },
-                {
-                    "rating": 4,
-                    "comment": "Very satisfied!",
-                    "date": "2025-04-30T09:41:02.053Z",
-                    "reviewerName": "Lucas Gordon",
-                    "reviewerEmail": "lucas.gordon@x.dummyjson.com"
-                },
-                {
-                    "rating": 5,
-                    "comment": "Highly impressed!",
-                    "date": "2025-04-30T09:41:02.053Z",
-                    "reviewerName": "Eleanor Collins",
-                    "reviewerEmail": "eleanor.collins@x.dummyjson.com"
-                }
-            ],
-            "returnPolicy": "No return policy",
-            "minimumOrderQuantity": 48,
-            "meta": {
-                "createdAt": "2025-04-30T09:41:02.053Z",
-                "updatedAt": "2025-04-30T09:41:02.053Z",
-                "barcode": "5784719087687",
-                "qrCode": "https://cdn.dummyjson.com/public/qr-code.png"
-            },
-            "images": [
-                "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
-            ],
-            "thumbnail": "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/thumbnail.webp"
-        }
+    const handleFileChange = (e) => {
+        setSelectedImages(e.target.files);
+    };
+    // const product = [
+    //     {
+    //         "id": 1,
+    //         "title": "Essence Mascara Lash Princess",
+    //         "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
+    //         "category": "beauty",
+    //         "price": 9.99,
+    //         "discountPercentage": 10.48,
+    //         "rating": 2.56,
+    //         "stock": 99,
+    //         "tags": [
+    //             "beauty",
+    //             "mascara"
+    //         ],
+    //         "brand": "Essence",
+    //         "sku": "BEA-ESS-ESS-001",
+    //         "weight": 4,
+    //         "dimensions": {
+    //             "width": 15.14,
+    //             "height": 13.08,
+    //             "depth": 22.99
+    //         },
+    //         "warrantyInformation": "1 week warranty",
+    //         "shippingInformation": "Ships in 3-5 business days",
+    //         "availabilityStatus": "In Stock",
+    //         "reviews": [
+    //             {
+    //                 "rating": 3,
+    //                 "comment": "Would not recommend!",
+    //                 "date": "2025-04-30T09:41:02.053Z",
+    //                 "reviewerName": "Eleanor Collins",
+    //                 "reviewerEmail": "eleanor.collins@x.dummyjson.com"
+    //             },
+    //             {
+    //                 "rating": 4,
+    //                 "comment": "Very satisfied!",
+    //                 "date": "2025-04-30T09:41:02.053Z",
+    //                 "reviewerName": "Lucas Gordon",
+    //                 "reviewerEmail": "lucas.gordon@x.dummyjson.com"
+    //             },
+    //             {
+    //                 "rating": 5,
+    //                 "comment": "Highly impressed!",
+    //                 "date": "2025-04-30T09:41:02.053Z",
+    //                 "reviewerName": "Eleanor Collins",
+    //                 "reviewerEmail": "eleanor.collins@x.dummyjson.com"
+    //             }
+    //         ],
+    //         "returnPolicy": "No return policy",
+    //         "minimumOrderQuantity": 48,
+    //         "meta": {
+    //             "createdAt": "2025-04-30T09:41:02.053Z",
+    //             "updatedAt": "2025-04-30T09:41:02.053Z",
+    //             "barcode": "5784719087687",
+    //             "qrCode": "https://cdn.dummyjson.com/public/qr-code.png"
+    //         },
+    //         "images": [
+    //             "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
+    //         ],
+    //         "thumbnail": "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/thumbnail.webp"
+    //     }
 
 
-    ]
-    const formik = useFormik({
-        initialValues: {
-            ProductName: '',
-            price: '',
-            categories: '',
-            image: '',
+    // ]
+    // const formik = useFormik({
+    //     initialValues: {
+    //         ProductName: '',
+    //         price: '',
+    //         categories: '',
+    //         image: '',
 
-        },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
+    //     },
+    //     onSubmit: values => {
+    //         alert(JSON.stringify(values, null, 2));
+    //     },
+    // });
     return (
         <div>
             <section>
@@ -152,17 +155,18 @@ const Products = () => {
                                     <Table striped bordered hover>
                                         <tbody>
                                             {
-                                                product ?
-                                                    product.map((product, index) => {
+                                                products ?
+                                                    products.map((product, index) => {
                                                         return (
                                                             <tr key={index}>
-
-                                                                <td><img src={product.thumbnail} className='xyz' /></td>
-                                                                <td>{product.title}</td>
+                                                                <td><img src={product.image} className='xyz' /></td>
+                                                                <td>{product.name}</td>
                                                                 <td>{product.price}</td>
+                                                                <td>{product.category}</td>
                                                                 <td><Link to={'/edit'}><Button variant="edit"><FaEdit /></Button></Link>
                                                                     <Link><Button variant="delete"><MdDeleteOutline /></Button></Link>
-                                                                    <Link to={'/view'}><Button variant="view"><FaRegEye /></Button></Link></td>
+                                                                    <Link to={'/view'}><Button variant="view"><FaRegEye /></Button></Link>
+                                                                </td>
 
                                                             </tr>
                                                         )
@@ -189,7 +193,7 @@ const Products = () => {
                                     onSubmit={async (values, { resetForm }) => {
                                         const formData = new FormData();
 
-                                          formData.append("userId", currentUser.id);
+                                        formData.append("userId", currentUser.id);
 
                                         Object.keys(values).forEach((key) => {
                                             formData.append(key, values[key]);
@@ -264,15 +268,14 @@ const Products = () => {
                                                         <option value="">Select Category</option>
                                                         {
                                                             categories ?
-                                                                categories.map((category,index)=>
-                                                                {
-                                                                    return(
+                                                                categories.map((category, index) => {
+                                                                    return (
                                                                         <option value={category.name} key={index}>{category.name}</option>
                                                                     )
                                                                 })
-                                                            :
+                                                                :
 
-                                                            "Not Available"
+                                                                "Not Available"
                                                         }
                                                     </Field>
                                                 </Col>
@@ -283,11 +286,11 @@ const Products = () => {
                                                     <input
                                                         type="file"
                                                         accept="image/*"
-                                                        onChange={(event) => {
-                                                            setFieldValue("image", event.currentTarget.files[0]);
-                                                        }}
+                                                        onChange={handleFileChange}
                                                     />
-                                                    {errors.image && touched.image && <p>{errors.image}</p>}
+                                                    {selectedImages.length > 0 && (
+                                                        <div>{selectedImages.length} image selected</div>
+                                                    )}
                                                 </Col>
                                             </Row>
                                             <button type="submit" className="button">
