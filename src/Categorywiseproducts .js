898a06cd7e76440c3efe02991b90e56330128135
truelivingvip <niteshmechanical629@gmail.com>
2026-04-05 20:18:55 +0530

    .

--- src/Categorywiseproducts .js
+++ src/Categorywiseproducts .js
@@ -5,6 +5,7 @@ import { useEffect } from 'react'
 import { Col, Container, Row, Breadcrumb, Card, Button, Table } from 'react-bootstrap'
 // import { MdDeleteOutline } from "react-icons/md";
 import { GoHeart } from "react-icons/go";
+import { FaOpencart } from "react-icons/fa";
 import Header from './Header';
 
 
@@ -25,45 +26,45 @@ const Categorywiseproducts = () => {
                 console.log("Error-fetching Data");
             });
     }, []);
-    const[wishlist,setWishlist] = useState([]);
+    const [wishlist, setWishlist] = useState([]);
     const user = JSON.parse(localStorage.getItem("user"));
-    const addToWishlist = async(productid) => {
-        try{
-            const res=await
-            fetch("http://localhost:8090/api/wishlist",{
-                method:"POST",
-                headers:{
-                    "Content-Type":"application/json"
-                },
-                body:JSON.stringify({
-                    userid: user._id,
-                    productid: productid
-                })
-            });
-            const data=await res.json();
+    const addToWishlist = async (productid) => {
+        try {
+            const res = await
+                fetch("http://localhost:8090/api/wishlist", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        userid: user._id,
+                        productid: productid
+                    })
+                });
+            const data = await res.json();
             console.log(data);
-        } catch (err){
+        } catch (err) {
             console.log(err);
-            }
-        };
-        // const exist = wishlist.find((item)=>item._id===product._id);
-        
-        // if(exist){
-        //     setWishlist(wishlist.filter((item) =>item._id!==product._id))
-        // } else{
-        //     setWishlist([...wishlist,product]);
-        // }
-        // console.log("Wishlist:",wishlist);
-        // axios
-        //     .post("http://localhost:8090/api/wishlist",{product})
-            // .then((res) => {
-            //     console.log("Successfully deleted");
-            //     window.location.reload()
-            // })
-            // .catch((error) => {
-            //     console.log("Error");
-            // });
-    
+        }
+    };
+    // const exist = wishlist.find((item)=>item._id===product._id);
+
+    // if(exist){
+    //     setWishlist(wishlist.filter((item) =>item._id!==product._id))
+    // } else{
+    //     setWishlist([...wishlist,product]);
+    // }
+    // console.log("Wishlist:",wishlist);
+    // axios
+    //     .post("http://localhost:8090/api/wishlist",{product})
+    // .then((res) => {
+    //     console.log("Successfully deleted");
+    //     window.location.reload()
+    // })
+    // .catch((error) => {
+    //     console.log("Error");
+    // });
+
     return (
         <div>
             <section>
@@ -83,13 +84,16 @@ const Categorywiseproducts = () => {
                             products ?
                                 products.map((product, index) => {
                                     return (
-                                        <Col key={index}>
-                                            <Card style={{ width: '18rem' }} className="img-container">
-                                                <Card.Img variant="top" src={`http://localhost:8090/uploads/${product.image}`} />
-                                                <Button onClick={() => addToWishlist(product._id)} variant="delete" className="delete-btn"><GoHeart color="red" className="delete-icon" /></Button>
-                                                <Card.Body>
-                                                    <Card.Title>{product.name}</Card.Title>
-                                                    <Button variant="primary">Shop Now</Button>
+                                        <Col key={index} className="mb-4">
+                                            <Card className="custom-card h-100 shadow-sm">
+                                                <div className='image-wrapper'>
+                                                    <Card.Img variant="top" src={`http://localhost:8090/uploads/${product.image}`} className='card-img-custom' />
+                                                    <div onClick={() => addToWishlist(product._id)} className="wishlist-btn"><GoHeart color="red" size={20}/></div>
+                                                </div>
+                                                <Card.Body className='d-flex flex-column'>
+                                                    <Card.Title className='product-title'>{product.name}</Card.Title>
+                                                    <button variant="primary" className='mt-auto shop-btn'>Shop Now</button>
+                                                    <button className="cart-btn"><FaOpencart size={30}/></button>
                                                 </Card.Body>
                                             </Card>
                                         </Col>
